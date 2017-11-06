4066be8cd4754cf9cb1f5b0b9badb3ba52981392
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 14:44:32 -0500

    Set data if no pokemon matches

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -4,9 +4,15 @@ class PokemonController < ApplicationController
   def show
     res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
     body = JSON.parse(res.body)
-    id = body["id"]
-    name = body["name"]
-    types = body["types"].map { |type| type["type"]["name"] }
+    if body == {"detail"=>"Not found."}
+      id = 0
+      name = "No pokemon found"
+      type = "none"
+    else
+      id = body["id"]
+      name = body["name"]
+      types = body["types"].map { |type| type["type"]["name"] }
+    end
 
     res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_API"]}&q=#{name}&rating=g")
     body = JSON.parse(res.body)
