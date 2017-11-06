2638b0792aa7d32c7d58d48f30d7e74aa803d972
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 14:14:04 -0500

    Display the JSON data for pokemon name, id, and types

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -4,9 +4,11 @@ class PokemonController < ApplicationController
   def show
     res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
     body = JSON.parse(res.body)
+    id = body["id"]
     name = body["name"]
+    types = body["types"].map { |type| type["type"]["name"] }
 
-    render json: { "message": "ok"}
+    render json: { "id": id, "name": name, "types": types}
   end
 
 end
