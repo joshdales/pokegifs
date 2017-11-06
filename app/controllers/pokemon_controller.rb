7e87c960fa546048e696bf1e30418920fc37dd42
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 14:33:31 -0500

    Include gif url in json data

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -8,10 +8,11 @@ class PokemonController < ApplicationController
     name = body["name"]
     types = body["types"].map { |type| type["type"]["name"] }
 
-    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_API"]}&q=pikachu&rating=g")
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_API"]}&q=#{name}&rating=g")
     body = JSON.parse(res.body)
-    
-    render json: { "id": id, "name": name, "types": types}
+    gif_url = body["data"][0]["url"]
+
+    render json: { "id": id, "name": name, "types": types, "gif": gif_url}
   end
 
 end
