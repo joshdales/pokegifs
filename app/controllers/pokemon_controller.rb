fa6b6e4fea614743cbe9a69cdc296fe549a387c9
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-06 13:55:24 -0500

    Set response and body in poekmon controller

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,6 +2,10 @@ require 'json'
 class PokemonController < ApplicationController
 
   def show
+    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(res.body)
+    name = body["name"]
+
     render json: { "message": "ok"}
   end
 
